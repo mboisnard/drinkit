6ccad4eab9787fb9540ed541a2da0ec002a03700
Mathieu Boisnard <mathieu.boisnard.pro@gmal.com>
2024-01-10 15:38:41 +0100

    [GRADLE] Improve openapi convention

--- buildSrc/src/main/kotlin/com.drinkit.contract-first.gradle.kts
+++ buildSrc/src/main/kotlin/com.drinkit.contract-first.gradle.kts
@@ -3,39 +3,64 @@ plugins {
     id("com.drinkit.common-conventions")
 }
 
+dependencies {
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+}
+
 val openApiInput: Configuration by configurations.creating {
+    isCanBeConsumed = false
+}
+
+val openApiCommonTemplates: Configuration by configurations.creating {
     isCanBeConsumed = false
 }
 
 // https://github.com/OpenAPITools/openapi-generator/tree/master/modules/openapi-generator-gradle-plugin
 openApiGenerate {
     generatorName.set("kotlin-spring")
-    inputSpec.set("${openApiInput.singleFile.path}/api-definition.yaml")
     outputDir.set(layout.buildDirectory.dir("generated-sources/openapi/src/main/kotlin").map { it.asFile.toString() })
+
+    // Remove old generated files before starting a new generation task
     cleanupOutput.set(true)
+
+    // Options for code generation depending on the generator used
+    // https://openapi-generator.tech/docs/generators/kotlin-spring/
     configOptions.set(
         mapOf(
-            "delegatePattern" to "true",
-            "useTags" to "true",
+            "delegatePattern" to "true", // Delegate pattern will create an interface that we can easily implement with a Spring service
+            "useTags" to "true", // Use the defined tags in the yaml file to create interfaces and class names
+            "useSpringBoot3" to "true", // Use jakarta annotations in generated code, jakarta libraries are imported by spring boot dependencies
             "sourceFolder" to "",
             "enumPropertyNaming" to "UPPERCASE"
         )
     )
-    typeMappings.set(
+
+    // Scope the generated files to only supportingFile/apis/models classes
+    // Can also be configured in .openapi-generator-ignore file
+    // https://openapi-generator.tech/docs/globals/
+    globalProperties.set(
         mapOf(
-            "string+bigdecimal" to "BigDecimal"
+            "supportingFiles" to "ApiUtil.kt",
+            "apis" to "",
+            "models" to "",
         )
     )
-    importMappings.set(
-        mapOf(
-            "BigDecimal" to "java.math.BigDecimal"
-        )
-    )
+
+    // Disable enum stripping on generated code
     additionalProperties.set(
         mapOf(
             "removeEnumValuePrefix" to false
         )
     )
+}
+
+tasks.openApiGenerate {
+    if (openApiInput.files.isNotEmpty()) {
+        inputSpec.set("${openApiInput.singleFile.path}/api-definition.yaml")
+    }
+
+    inputs.files(openApiCommonTemplates)
+    inputs.files(openApiInput)
 }
 
 kotlin {
--- drinkit-backend/build.gradle.kts
+++ drinkit-backend/build.gradle.kts
@@ -1,15 +1,17 @@
 plugins {
     id("com.drinkit.api-conventions")
-    //id("com.drinkit.contract-first")
+    id("com.drinkit.contract-first")
 }
 
 dependencies {
+
     implementation(project(":drinkit-domain"))
     implementation(project(":drinkit-infra"))
 }
 
-/*
+
 openApiGenerate {
+    inputSpec.set("$rootDir/drinkit-api-contract/contract/api-definition.yaml")
     apiPackage.set("com.drinkit.api.generated.api")
     modelPackage.set("com.drinkit.api.generated.model")
-}*/
+}
