08fc9d3107f2e36cff40d68951294ca9267d01b5
Mathieu Boisnard <mathieu.boisnard@malt.com>
2024-01-19 18:57:31 +0100

    [JOOQ] Introduce Jooq codegen plugin + add jooq codegen files to SCV to avoid starting local docker db + jooqCodeGen gradle task before being able to start any kind of dev

--- drinkit/drinkit-infra/build.gradle.kts
+++ drinkit/drinkit-infra/build.gradle.kts
@@ -6,7 +6,13 @@ plugins {
 
 dependencies {
     implementation(project(":drinkit-domain"))
+
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.postgresql:postgresql")
+    implementation(libs.jooq)
     implementation(libs.bson)
+
+    jooqCodegen("org.postgresql:postgresql")
 }
 
 jooq {
@@ -14,8 +20,20 @@ jooq {
         generator {
             name = "org.jooq.codegen.KotlinGenerator"
 
+            jdbc {
+                driver = "org.postgresql.Driver"
+                url = "jdbc:postgresql://localhost:5432/drinkit"
+                username = "drinkit"
+                password = "admin"
+            }
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                includes = "cellar"
+                inputSchema = "public"
+            }
             target {
                 packageName = "com.drinkit.generated.jooq"
+                directory = "src/generated/jooq/kotlin"
             }
         }
     }
