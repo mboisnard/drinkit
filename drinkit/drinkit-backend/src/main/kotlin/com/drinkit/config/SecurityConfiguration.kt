c55cddd61a657e23a9a33676eef28159fb1c2d6f
Mathieu Boisnard <mathieu.boisnard@malt.com>
2024-01-23 10:21:35 +0100

    [SECURITY] Use the security context repository instead saving manually the context into the session (https://docs.spring.io/spring-security/reference/servlet/authentication/persistence.html)

--- drinkit/drinkit-backend/src/main/kotlin/com/drinkit/config/SecurityConfiguration.kt
+++ drinkit/drinkit-backend/src/main/kotlin/com/drinkit/config/SecurityConfiguration.kt
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository
+import org.springframework.security.web.context.SecurityContextRepository
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.COOKIES
 
@@ -26,7 +28,7 @@ class SecurityConfiguration {
 
     @Bean
     @Throws(Exception::class)
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    fun filterChain(http: HttpSecurity, securityContextRepository: SecurityContextRepository): SecurityFilterChain {
         http.csrf { it.disable() }
             .authorizeHttpRequests {
                 it
@@ -35,6 +37,9 @@ class SecurityConfiguration {
                     .requestMatchers("/actuator/**").hasRole("USER")
                     .requestMatchers("/openapi/**").hasRole("USER")
                     .anyRequest().denyAll()
+            }
+            .securityContext {
+                it.securityContextRepository(securityContextRepository)
             }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.ALWAYS).maximumSessions(1) }
             //.headers { it.frameOptions { it.sameOrigin() } }
@@ -67,4 +72,7 @@ class SecurityConfiguration {
     fun authenticationManager(
         configuration: AuthenticationConfiguration
     ): AuthenticationManager = configuration.authenticationManager
+
+    @Bean
+    fun sessionContextRepository(): SecurityContextRepository = HttpSessionSecurityContextRepository()
 }
--- drinkit/drinkit-backend/src/main/kotlin/com/drinkit/user/AuthApi.kt
+++ drinkit/drinkit-backend/src/main/kotlin/com/drinkit/user/AuthApi.kt
@@ -3,19 +3,22 @@ package com.drinkit.user
 import com.drinkit.api.generated.api.AuthApiDelegate
 import com.drinkit.api.generated.model.SignInRequest
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY
+import org.springframework.security.web.context.SecurityContextRepository
 import org.springframework.stereotype.Service
 
 @Service
 internal class AuthApi(
     private val request: HttpServletRequest?,
+    private val response: HttpServletResponse?,
     private val authenticationManager: AuthenticationManager,
+    private val securityContextRepository: SecurityContextRepository,
 ): AuthApiDelegate {
 
     override fun authenticateUser(signInRequest: SignInRequest): ResponseEntity<Unit> {
@@ -27,8 +30,7 @@ internal class AuthApi(
             val securityContext = SecurityContextHolder.getContext()
             securityContext.authentication = authentication
 
-            val session = request!!.getSession(true)
-            session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext)
+            securityContextRepository.saveContext(securityContext, request!!, response!!)
 
             return ResponseEntity.ok().build()
         } catch (ex: AuthenticationException) {
