6a0e67e409c8345244fe8bc27b390101b4cf9a2b
Mathieu Boisnard <mathieu.boisnard@malt.com>
2024-03-02 23:50:38 +0100

    [TEST] Add cellar fixtures

---
+++ drinkit/drinkit-domain/src/testFixtures/kotlin/com/drinkit/cellar/CellarFixtures.kt
@@ -0,0 +1,30 @@
+package com.drinkit.cellar
+
+import com.drinkit.common.ControlledClock
+import com.drinkit.common.ControlledIdGenerator
+import com.drinkit.messaging.SpyEventPublisher
+
+class CellarFixtures {
+
+    val controlledIdGenerator = ControlledIdGenerator()
+
+    val spyEventPublisher = SpyEventPublisher()
+
+    val controlledClock = ControlledClock()
+
+    val cellars = InMemoryCellars()
+
+    val createCellar = CreateCellar(
+        cellars = cellars,
+        idGenerator = controlledIdGenerator,
+    )
+
+    val deleteCellar = DeleteCellar(
+        cellars = cellars,
+        readCellars = cellars,
+    )
+
+    val findCellars = FindCellars(
+        cellars = cellars,
+    )
+}
---
+++ drinkit/drinkit-domain/src/testFixtures/kotlin/com/drinkit/cellar/InMemoryCellars.kt
@@ -0,0 +1,24 @@
+package com.drinkit.cellar
+
+import com.drinkit.user.UserId
+class InMemoryCellars: ReadCellars, WriteCellars {
+
+    private var cellars: MutableMap<CellarId, Cellar> = mutableMapOf()
+    override fun create(cellar: Cellar): CellarId {
+        if (!cellars.containsKey(cellar.id)) {
+            cellars[cellar.id] = cellar
+            return cellar.id
+        }
+
+        throw IllegalStateException("Cellar already exists")
+    }
+
+    override fun delete(cellarId: CellarId) {
+        cellars.remove(cellarId)
+    }
+
+    override fun findById(cellarId: CellarId): Cellar? = cellars[cellarId]
+
+    override fun findAllByOwnerId(owner: UserId): Sequence<Cellar> =
+        cellars.filter { it.value.owner == owner }.values.asSequence()
+}
