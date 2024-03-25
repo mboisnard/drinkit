5b2bb40b4faf8337c4aac8e8e357ec616bd35980
Mathieu Boisnard <mathieu.boisnard@malt.com>
2024-03-25 20:45:46 +0100

    [GITHUB] Configure code analysis with Detekt

--- buildSrc/src/main/kotlin/com.drinkit.code-analysis-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.drinkit.code-analysis-conventions.gradle.kts
@@ -1,12 +1,13 @@
 import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 
 plugins {
     id("io.gitlab.arturbosch.detekt")
 }
 
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.5")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
 }
 
 detekt {
@@ -16,12 +17,29 @@ detekt {
     //autoCorrect = true
     config.setFrom("$rootDir/code-analysis/detekt/detekt.yml")
     baseline = file("$rootDir/code-analysis/detekt/baseline.xml")
+    basePath = rootProject.projectDir.absolutePath
+}
+
+
+// Create a dedicate task to merge all reports from each gradle project
+// https://detekt.dev/docs/introduction/reporting/#merging-reports
+val detektReportMergeSarif by tasks.registering(ReportMergeTask::class) {
+    output = layout.buildDirectory.file("reports/detekt/detekt-all-projects.sarif")
 }
 
 tasks.withType<Detekt>().configureEach {
     jvmTarget = "1.8"
+    reports {
+        html.required.set(true)
+        sarif.required.set(true)
+    }
+    finalizedBy(detektReportMergeSarif)
 }
 
 tasks.withType<DetektCreateBaselineTask>().configureEach {
     jvmTarget = "1.8"
+}
+
+detektReportMergeSarif {
+    input.from(tasks.withType<Detekt>().map { it.sarifReportFile })
 }
