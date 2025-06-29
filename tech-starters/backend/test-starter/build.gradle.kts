d78806d5ca0ecc309acd5d8cf576f3260d603e63
Mathieu Boisnard <mathieu.boisnard@malt.com>
2025-06-29 12:36:59 +0200

    [VERSION-UPDATE] Update spring boot to 3.5.3

--- tech-starters/backend/test-starter/build.gradle.kts
+++ tech-starters/backend/test-starter/build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     api(libs.io.rest.assured)
 
     api("org.springframework.boot:spring-boot-starter-test")
+    api("org.jetbrains.kotlin:kotlin-test-junit5")
+    runtimeOnly("org.junit.platform:junit-platform-launcher")
+
     api("org.springframework.boot:spring-boot-testcontainers")
     api("org.testcontainers:junit-jupiter")
 }
