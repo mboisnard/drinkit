41aeb7cd4cf61ed6b0c6f1fc7b00c4f631e71f4b
Mathieu Boisnard <mathieu.boisnard@malt.com>
2025-07-03 21:59:21 +0200

    [DOCUMENTATION] Remove custom template, add base option

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   srcDir: 'src',
+  base: '/drinkit/',
 
   title: "Drink It",
   description: "Cellar management for beer, wine, spirits",
