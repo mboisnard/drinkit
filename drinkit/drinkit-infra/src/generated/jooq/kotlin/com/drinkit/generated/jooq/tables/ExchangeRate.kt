3897b3ad4b28983b47f794e1b48fa81bedcbbb85
Mathieu Boisnard <esp010@hotmail.fr>
2026-09-12 21:16:41 +0200

    [TECH-STARTER] Generate jooq classes after version dump

--- drinkit/drinkit-infra/src/generated/jooq/kotlin/com/drinkit/generated/jooq/tables/ExchangeRate.kt
+++ drinkit/drinkit-infra/src/generated/jooq/kotlin/com/drinkit/generated/jooq/tables/ExchangeRate.kt
@@ -23,13 +23,14 @@ import org.jooq.QueryPart
 import org.jooq.Record
 import org.jooq.SQL
 import org.jooq.Schema
-import org.jooq.Select
 import org.jooq.Stringly
 import org.jooq.Table
 import org.jooq.TableField
+import org.jooq.TableLike
 import org.jooq.TableOptions
 import org.jooq.UniqueKey
 import org.jooq.impl.DSL
+import org.jooq.impl.Internal
 import org.jooq.impl.SQLDataType
 import org.jooq.impl.TableImpl
 
@@ -138,7 +139,7 @@ open class ExchangeRate(
     /**
      * Create an inline derived table from this table
      */
-    override fun where(condition: Condition?): ExchangeRate = ExchangeRate(qualifiedName, if (aliased()) this else null, condition)
+    override fun where(condition: Condition?): ExchangeRate = ExchangeRate(qualifiedName, if (aliased()) this else null, Internal.condition(this, condition))
 
     /**
      * Create an inline derived table from this table
@@ -178,10 +179,10 @@ open class ExchangeRate(
     /**
      * Create an inline derived table from this table
      */
-    override fun whereExists(select: Select<*>): ExchangeRate = where(DSL.exists(select))
+    override fun whereExists(select: TableLike<*>): ExchangeRate = where(DSL.exists(select))
 
     /**
      * Create an inline derived table from this table
      */
-    override fun whereNotExists(select: Select<*>): ExchangeRate = where(DSL.notExists(select))
+    override fun whereNotExists(select: TableLike<*>): ExchangeRate = where(DSL.notExists(select))
 }
--- tech-starters/backend/configuration-starter/src/generated/jooq/kotlin/com/drinkit/configuration/generated/jooq/tables/Configuration.kt
+++ tech-starters/backend/configuration-starter/src/generated/jooq/kotlin/com/drinkit/configuration/generated/jooq/tables/Configuration.kt
@@ -23,13 +23,14 @@ import org.jooq.QueryPart
 import org.jooq.Record
 import org.jooq.SQL
 import org.jooq.Schema
-import org.jooq.Select
 import org.jooq.Stringly
 import org.jooq.Table
 import org.jooq.TableField
+import org.jooq.TableLike
 import org.jooq.TableOptions
 import org.jooq.UniqueKey
 import org.jooq.impl.DSL
+import org.jooq.impl.Internal
 import org.jooq.impl.SQLDataType
 import org.jooq.impl.TableImpl
 
@@ -131,7 +132,7 @@ open class Configuration(
     /**
      * Create an inline derived table from this table
      */
-    override fun where(condition: Condition?): Configuration = Configuration(qualifiedName, if (aliased()) this else null, condition)
+    override fun where(condition: Condition?): Configuration = Configuration(qualifiedName, if (aliased()) this else null, Internal.condition(this, condition))
 
     /**
      * Create an inline derived table from this table
@@ -171,10 +172,10 @@ open class Configuration(
     /**
      * Create an inline derived table from this table
      */
-    override fun whereExists(select: Select<*>): Configuration = where(DSL.exists(select))
+    override fun whereExists(select: TableLike<*>): Configuration = where(DSL.exists(select))
 
     /**
      * Create an inline derived table from this table
      */
-    override fun whereNotExists(select: Select<*>): Configuration = where(DSL.notExists(select))
+    override fun whereNotExists(select: TableLike<*>): Configuration = where(DSL.notExists(select))
 }
